a270517b3a5314e988cf4886152ea0f7d9bd1128
mateusz.ozimek91 <mateusz.ozimek91@gmail.com>
2020-04-04 21:00:56 +0200

    Dzienniczek ucznia

---
+++ Diary.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace StudentDiary
+{
+    class Diary
+    {
+
+        //stan
+        List<float> ratings;
+
+
+        //zachowania
+        public void AddRating(float rating)
+        {
+            ratings.Add(rating);
+        }
+
+        public float CalculateAverage()
+        {
+            float sum = 0, avg = 0;
+
+            foreach (var rating in ratings)
+            {
+                sum = sum + rating;
+            }
+
+            avg = sum / ratings.Count();
+
+            return avg;
+               
+        }
+
+        public float GiveMaxRating()
+        {
+            return ratings.Max();
+        }
+
+        public float GiveMinRating()
+        {
+            return ratings.Min();
+        }
+    }
+}
